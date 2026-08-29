20b49e9e980d4484ebf2c087a228b116684b4be2
meoacgx <a1478882500@gmail.com>
2026-08-29 18:45:49 +0800

    fix(user): restore user search type contract

--- model/user.go
+++ model/user.go
@@ -445,6 +445,18 @@ func GetAllUsers(pageInfo *common.PageInfo, sortOptions ...UserSortOptions) (use
 }
 
 func SearchUsers(keyword string, group string, role *int, status *int, startIdx int, num int, sortOptions ...UserSortOptions) ([]*User, int64, error) {
+	return searchUsers(keyword, group, role, status, startIdx, num, resolveUserSortOptions(sortOptions), "all")
+}
+
+func SearchUsersWithSort(keyword string, group string, role *int, status *int, startIdx int, num int, sortOptions UserSortOptions, searchTypes ...string) ([]*User, int64, error) {
+	searchType := "all"
+	if len(searchTypes) > 0 {
+		searchType = strings.ToLower(strings.TrimSpace(searchTypes[0]))
+	}
+	return searchUsers(keyword, group, role, status, startIdx, num, sortOptions, searchType)
+}
+
+func searchUsers(keyword string, group string, role *int, status *int, startIdx int, num int, sortOptions UserSortOptions, searchType string) ([]*User, int64, error) {
 	var users []*User
 	var total int64
 	var err error
@@ -463,19 +475,29 @@ func SearchUsers(keyword string, group string, role *int, status *int, startIdx 
 	// 构建基础查询
 	query := tx.Unscoped().Model(&User{})
 
-	// 构建搜索条件
-	likeCondition := "username LIKE ? OR email LIKE ? OR display_name LIKE ?"
-	likeArgs := []interface{}{"%" + keyword + "%", "%" + keyword + "%", "%" + keyword + "%"}
-
-	// 尝试将关键字转换为整数ID
-	keywordInt, err := strconv.Atoi(keyword)
-	if err == nil {
-		// 如果是数字，同时搜索ID和其他字段
-		likeCondition = "id = ? OR " + likeCondition
-		likeArgs = append([]interface{}{keywordInt}, likeArgs...)
+	keyword = strings.TrimSpace(keyword)
+	if keyword != "" {
+		likeKeyword := "%" + keyword + "%"
+		switch searchType {
+		case "id":
+			keywordInt, parseErr := strconv.ParseInt(keyword, 10, 32)
+			if parseErr != nil || keywordInt <= 0 {
+				query = query.Where("1 = 0")
+			} else {
+				query = query.Where("id = ?", int(keywordInt))
+			}
+		case "username":
+			query = query.Where("username LIKE ?", likeKeyword)
+		default:
+			likeCondition := "username LIKE ? OR email LIKE ? OR display_name LIKE ?"
+			likeArgs := []interface{}{likeKeyword, likeKeyword, likeKeyword}
+			if keywordInt, parseErr := strconv.ParseInt(keyword, 10, 32); parseErr == nil && keywordInt > 0 {
+				likeCondition = "id = ? OR " + likeCondition
+				likeArgs = append([]interface{}{int(keywordInt)}, likeArgs...)
+			}
+			query = query.Where("("+likeCondition+")", likeArgs...)
+		}
 	}
-
-	query = query.Where("("+likeCondition+")", likeArgs...)
 	if group != "" {
 		query = query.Where(commonGroupCol+" = ?", group)
 	}
@@ -498,7 +520,7 @@ func SearchUsers(keyword string, group string, role *int, status *int, startIdx 
 	}
 
 	// 获取分页数据
-	order := resolveUserSortOptions(sortOptions)
+	order := sortOptions
 	err = order.Apply(query.Omit("password", "access_token")).Limit(num).Offset(startIdx).Find(&users).Error
 	if err != nil {
 		tx.Rollback()
